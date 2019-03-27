312af6e8c6c6b61e097a4fa64b851261c6c7f112
conectado <gabrielalejandro7@gmail.com>
2019-03-26 22:15:49 -0300

    Add ns_solver API definition

---
+++ src/ns_solver.h
@@ -0,0 +1,26 @@
+#ifndef __NS_SOLVER_H_INCLUDED__
+#define __NS_SOLVER_H_INCLUDED__
+template <class matrix>
+class ns_solver {
+  private:
+    int itermax;
+
+    double delX, delY;
+    int imax, jmax;
+    matrix u, v, p, F, G;
+
+  public:
+    ns_solver ();
+    ns_solver (double initial, double del, double size);
+    ns_solver (double initial, double delX, double sizeX, double delY, double sizeY);
+
+    void evolve();
+    void evolve(double dt);
+
+    // TODO make N-dimension available
+    double& operator()(const int i, const int j);
+    double operator()(const int i, const int j) const;
+
+    ~ns_solver();
+}; 
+#endif
